f0f9b66259bb795893f204d0ebf5b0dca850501d
Matthew Spelman <matthewspelman@gmail.com>
2025-03-16 21:56:39 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`scroll-smooth ${inter.variable}`}>
+      <head>
+        <link rel="icon" type="image/x-icon" href="/icons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white antialiased">
         {children}
       </body>
